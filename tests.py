0f5b112f169f9cf98dfa2f3c907ab4a75b1ffc92
vintagedave <vintagedave@gmail.com>
2023-12-17 15:34:11 +0700

    Tests: added many more tests wrapping the general method to pass in a set of files, and fixed tests according to correct output
    Note all fonts contain one ".notdef" glyph, so all counts 1 higher than expected. Some also contain ligatures etc

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
-from fontimize import get_used_characters_in_html, charPair, _get_char_ranges, optimise_fonts
-from fontTools.ttLib import TTFont
+from fontimize import get_used_characters_in_html, charPair, _get_char_ranges, optimise_fonts, optimise_fonts_for_files
+from fontTools.ttLib import woff2, TTFont
 
 class TestGetUsedCharactersInHtml(unittest.TestCase):
     def test_empty_html(self):
@@ -58,25 +58,32 @@ class TestCharRanges(unittest.TestCase):
         self.assertEqual(_get_char_ranges(['a', 'b', 'd', 'e', 'f', 'h']), [charPair('a', 'b'), charPair('d', 'f'), charPair('h', 'h')])
 
 
+# Used to verify the number of glyphs in a font matches the number of (unique!) characters in the test string
+def _count_glyphs_in_font(fontpath):
+    # with open(fontpath, 'rb') as f:
+    # wfr = woff2.WOFF2Reader(f)
+    # cmap = font['cmap']
+    # return len(cmap.getBestCmap())
+    # font.flavor = None  # Decompress the font data
+    font = TTFont(fontpath)#flavor='woff2')#, sfntReader=wfr)
+    font.flavor = None  # Decompress the font data
+    num_glyphs = font['maxp'].numGlyphs # Use font.getGlyphOrder() and https://fontdrop.info to examine, if weird
+    return num_glyphs
+
 class TestOptimiseFonts(unittest.TestCase):
     # Contains unique characters, none repeated, a couple of capitals, some symbols, and 26 lowercase
     test_string = " ,.@QT_abcdefghijklmnopqrstuvwxyz"
 
-    # Used to verify the number of glyphs in a font matches the number of (unique!) characters in the test string
-    def _count_glyphs_in_font(self, fontpath):
-        font = TTFont(fontpath)
-        cmap = font['cmap']
-        return len(cmap.getBestCmap())
-
     def test_optimise_fonts_with_single_font(self):
-        result = optimise_fonts(self.test_string, ['tests/Spirax-Regular.ttf'], fontpath='tests/output')
+        result = optimise_fonts(self.test_string, ['tests/Spirax-Regular.ttf'], fontpath='tests/output', verbose=False, print_stats=False)
         # Basics
         self.assertIsInstance(result, dict)
         self.assertIn('tests/Spirax-Regular.ttf', result)
         # Generated with the right name
         self.assertEqual(result['tests/Spirax-Regular.ttf'], 'tests/output/Spirax-Regular.FontimizeSubset.woff2')
         # If the number of glyphs in the font matches the expected number
-        self.assertEqual(len(self.test_string), self._count_glyphs_in_font(result['tests/Spirax-Regular.ttf']))
+        # For +1, see test_optimise_fonts_with_empty_text
+        self.assertEqual(len(self.test_string) + 1, _count_glyphs_in_font(result['tests/Spirax-Regular.ttf']))
 
     def test_optimise_fonts_with_multiple_fonts(self):
         result = optimise_fonts(self.test_string,
@@ -90,20 +97,71 @@ class TestOptimiseFonts(unittest.TestCase):
         self.assertIn('tests/EBGaramond-Italic-VariableFont_wght.ttf', result)
         self.assertEqual(result['tests/EBGaramond-Italic-VariableFont_wght.ttf'], 'tests/output/EBGaramond-Italic-VariableFont_wght.FontimizeSubset.woff2')
         # If the number of glyphs in the font matches the expected number
-        self.assertEqual(len(self.test_string), self._count_glyphs_in_font(result['tests/Spirax-Regular.ttf']))
-        self.assertEqual(len(self.test_string), self._count_glyphs_in_font(result['tests/EBGaramond-VariableFont_wght.ttf']))
-        self.assertEqual(len(self.test_string), self._count_glyphs_in_font(result['tests/EBGaramond-Italic-VariableFont_wght.ttf']))
+        # + 1 for the tests below -- see test_optimise_fonts_with_empty_text
+        self.assertEqual(len(self.test_string) + 1, _count_glyphs_in_font('tests/output/Spirax-Regular.FontimizeSubset.woff2'))
+        # + 16, + 12: EB Garamond contains multiple f-ligatures (eg fi), plus other variants, so the number of glyphs is higher. Italic has fewer.
+        self.assertEqual(len(self.test_string) + 1 + 16, _count_glyphs_in_font('tests/output/EBGaramond-VariableFont_wght.FontimizeSubset.woff2'))
+        self.assertEqual(len(self.test_string) + 1 + 12, _count_glyphs_in_font('tests/output/EBGaramond-Italic-VariableFont_wght.FontimizeSubset.woff2'))
 
     def test_optimise_fonts_with_empty_text(self):
         result = optimise_fonts("",
             ['tests/Spirax-Regular.ttf'],
-            fontpath='tests/output')
+            fontpath='tests/output',
+            verbose=False, print_stats=False)
         self.assertIsInstance(result, dict)
         self.assertIn('tests/Spirax-Regular.ttf', result)
         self.assertEqual(result['tests/Spirax-Regular.ttf'], 'tests/output/Spirax-Regular.FontimizeSubset.woff2')
-        # If the number of glyphs in the font matches the expected number: one, because an empty string is reported as containing space, see get_used_characters_in_str
-        self.assertEqual(1, self._count_glyphs_in_font(result['tests/Spirax-Regular.ttf']))
+        # If the number of glyphs in the font matches the expected number: two, because an empty string is reported as containing space, see get_used_characters_in_str
+        # and fonts also seem to contain ".notdef":
+        #   > font.getGlyphOrder()
+        #   > ['.notdef', 'space']
+        self.assertEqual(2, _count_glyphs_in_font('tests/output/Spirax-Regular.FontimizeSubset.woff2'))
+
+
+class TestOptimiseFontsForFiles(unittest.TestCase):
+
+    def setUp(self):
+        self.files = ['tests/test1-index-css.html', 'tests/test.txt', 'tests/test2.html']
+        self.font_output_dir = 'output'
+        self.subsetname = 'TestFilesSubset'
+        self.verbose = False
+        self.print_stats = False
+        self.fonts = ['tests/Whisper-Regular.ttf'] # Not used by any HTML/CSS, mimics manually adding a font
+    
+    def test_optimise_fonts_for_files(self):
+        result = optimise_fonts_for_files(files=self.files, font_output_dir=self.font_output_dir, subsetname=self.subsetname, fonts=self.fonts,
+            verbose=False, print_stats=False)
         
+        self.assertIsInstance(result, dict)
+        self.assertIn('css', result)
+        self.assertIn('fonts', result)
+        
+        css = result['css']
+        self.assertIn('tests/css_test.css', css)
+        self.assertIn('tests/css_test-index.css', css)
+        self.assertEqual(len(css), 2)
+
+        fonts = result['fonts']
+        font_keys = fonts.keys()
+        self.assertIn('tests/EBGaramond-Italic-VariableFont_wght.ttf', font_keys) # These four found in CSS, via HTML input
+        self.assertIn('tests/EBGaramond-VariableFont_wght.ttf', font_keys)
+        self.assertIn('tests/Spirax-Regular.ttf', font_keys)
+        self.assertIn('tests/SortsMillGoudy-Regular.ttf', font_keys)
+        self.assertIn('tests/Whisper-Regular.ttf', font_keys) # Manually specified
+        self.assertEqual(len(fonts), 5)
+
+        self.assertDictEqual(fonts, 
+                             {
+                                'tests/EBGaramond-Italic-VariableFont_wght.ttf': 'tests/output/EBGaramond-Italic-VariableFont_wght.TestFilesSubset.woff2',
+                                'tests/EBGaramond-VariableFont_wght.ttf': 'tests/output/EBGaramond-VariableFont_wght.TestFilesSubset.woff2',
+                                'tests/Spirax-Regular.ttf': 'tests/output/Spirax-Regular.TestFilesSubset.woff2',
+                                'tests/SortsMillGoudy-Regular.ttf': 'tests/output/SortsMillGoudy-Regular.TestFilesSubset.woff2',
+                                'tests/Whisper-Regular.ttf': 'tests/output/Whisper-Regular.TestFilesSubset.woff2'
+                             }
+                            )
+        
+        # Check some glyphs
+        self.assertEqual(2, _count_glyphs_in_font(result['tests/output/Spirax-Regular.TestFilesSubset.woff2']))
 
 if __name__ == '__main__':
     unittest.main()
